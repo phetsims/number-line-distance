0f8ccca28a05d3bc136dab69126476fc98a9aa3e
samreid <reids@colorado.edu>
2021-03-11 16:33:35 -0700

    Enable "no-floating-decimal" rule, see https://github.com/phetsims/chipper/issues/814

--- js/explore/view/NLDExploreScreenView.js
+++ js/explore/view/NLDExploreScreenView.js
@@ -61,7 +61,7 @@ class NLDExploreScreenView extends ScreenView {
         bottom: this.layoutBounds.maxY - 107,
         baseColor: 'white',
         selectedLineWidth: 2,
-        deselectedLineWidth: .5,
+        deselectedLineWidth: 0.5,
         deselectedButtonOpacity: 0.25,
         orientation: 'horizontal',
         spacing: 7
