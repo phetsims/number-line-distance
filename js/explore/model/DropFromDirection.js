388e783f199e313912b1415ed961286f9cbf950a
phet-dev <phetadmin@gmail.com>
2021-07-11 03:39:21 -0600

    update copyright dates from daily grunt work

--- js/explore/model/DropFromDirection.js
+++ js/explore/model/DropFromDirection.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2021, University of Colorado Boulder
 
 /**
  * Represents from where a point controller should 'drop' onto a number line for #34.
