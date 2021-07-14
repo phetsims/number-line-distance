11a17ba072b387fddceda898f0c2f4d6fe533179
Saurabh Totey <SaurabhTotey@gmail.com>
2021-07-14 17:01:35 +0200

    DistanceStatementNode comment
    See #11

--- js/common/view/DistanceStatementNode.js
+++ js/common/view/DistanceStatementNode.js
@@ -2,11 +2,9 @@
 
 /**
  * A generic distance statement as a scenery Node. Can either be set to be able to control point controller values
- * (using number pickers) or just show values (using texts).
- * All minus symbols use the MathSymbols.MINUS symbol (even if it is a unary minus) so that all the minus symbols are
- * consistent (see #27).
- * Doesn't need to unlink properties because all instances of
- * DistanceStatementNode are present for the lifetime of the sim.
+ * (using number pickers) or just show values (using texts). All minus symbols use the MathSymbols.MINUS symbol (even if
+ * it is a unary minus) so that all the minus symbols are consistent (see #27). Doesn't need to unlink properties because
+ * all instances of DistanceStatementNode are present for the lifetime of the sim.
  *
  * @author Saurabh Totey
  */
