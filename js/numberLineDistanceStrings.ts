09738af7e9728b64cae86f34aec3f8a5e1688e50
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:35 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/numberLineDistanceStrings.ts
@@ -0,0 +1,63 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import numberLineDistance from './numberLineDistance.js';
+
+type StringsType = {
+  'number-line-distance': {
+    'title': string
+  },
+  'screen': {
+    'explore': string,
+    'generic': string
+  },
+  'pointLabels': string,
+  'distanceLabels': string,
+  'distanceDescription': string,
+  'distanceStatement': string,
+  'tickMarks': string,
+  'absoluteValue': string,
+  'directedDistance': string,
+  'symbol': {
+    'x': string,
+    'y': string,
+    'east': string,
+    'west': string,
+    'meters': string,
+    'degreesCelsius': string,
+    'A': string,
+    'B': string
+  },
+  'unit': string,
+  'units': string,
+  'genericAbsoluteDistanceTemplate': string,
+  'genericDirectedPositiveDistanceTemplate': string,
+  'genericDirectedNegativeDistanceTemplate': string,
+  'person': string,
+  'house': string,
+  'meter': string,
+  'meters': string,
+  'distanceSceneAbsoluteDistanceTemplate': string,
+  'distanceSceneDirectedPositiveDistanceTemplate': string,
+  'distanceSceneDirectedNegativeDistanceTemplate': string,
+  'degree': string,
+  'degrees': string,
+  'temperatureSceneAbsoluteDistanceTemplate': string,
+  'temperatureSceneDirectedPositiveDistanceTemplate': string,
+  'temperatureSceneDirectedNegativeDistanceTemplate': string,
+  'fish': string,
+  'bird': string,
+  'elevationSceneAbsoluteDistanceTemplate': string,
+  'elevationSceneDirectedPositiveDistanceTemplate': string,
+  'elevationSceneDirectedNegativeDistanceTemplate': string
+};
+
+const numberLineDistanceStrings = getStringModule( 'NUMBER_LINE_DISTANCE' ) as StringsType;
+
+numberLineDistance.register( 'numberLineDistanceStrings', numberLineDistanceStrings );
+
+export default numberLineDistanceStrings;
