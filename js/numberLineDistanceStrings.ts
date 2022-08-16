61ca9918ed5ec6b71d51a4f65245222e6a84e81e
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:35 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/numberLineDistanceStrings.ts
+++ js/numberLineDistanceStrings.ts
@@ -5,55 +5,96 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import numberLineDistance from './numberLineDistance.js';
 
 type StringsType = {
   'number-line-distance': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'explore': string;
+    'exploreProperty': TReadOnlyProperty<string>;
     'generic': string;
+    'genericProperty': TReadOnlyProperty<string>;
   };
   'pointLabels': string;
+  'pointLabelsProperty': TReadOnlyProperty<string>;
   'distanceLabels': string;
+  'distanceLabelsProperty': TReadOnlyProperty<string>;
   'distanceDescription': string;
+  'distanceDescriptionProperty': TReadOnlyProperty<string>;
   'distanceStatement': string;
+  'distanceStatementProperty': TReadOnlyProperty<string>;
   'tickMarks': string;
+  'tickMarksProperty': TReadOnlyProperty<string>;
   'absoluteValue': string;
+  'absoluteValueProperty': TReadOnlyProperty<string>;
   'directedDistance': string;
+  'directedDistanceProperty': TReadOnlyProperty<string>;
   'symbol': {
     'x': string;
+    'xProperty': TReadOnlyProperty<string>;
     'y': string;
+    'yProperty': TReadOnlyProperty<string>;
     'east': string;
+    'eastProperty': TReadOnlyProperty<string>;
     'west': string;
+    'westProperty': TReadOnlyProperty<string>;
     'meters': string;
+    'metersProperty': TReadOnlyProperty<string>;
     'degreesCelsius': string;
+    'degreesCelsiusProperty': TReadOnlyProperty<string>;
     'A': string;
+    'AProperty': TReadOnlyProperty<string>;
     'B': string;
+    'BProperty': TReadOnlyProperty<string>;
   };
   'unit': string;
+  'unitProperty': TReadOnlyProperty<string>;
   'units': string;
+  'unitsProperty': TReadOnlyProperty<string>;
   'genericAbsoluteDistanceTemplate': string;
+  'genericAbsoluteDistanceTemplateProperty': TReadOnlyProperty<string>;
   'genericDirectedPositiveDistanceTemplate': string;
+  'genericDirectedPositiveDistanceTemplateProperty': TReadOnlyProperty<string>;
   'genericDirectedNegativeDistanceTemplate': string;
+  'genericDirectedNegativeDistanceTemplateProperty': TReadOnlyProperty<string>;
   'person': string;
+  'personProperty': TReadOnlyProperty<string>;
   'house': string;
+  'houseProperty': TReadOnlyProperty<string>;
   'meter': string;
+  'meterProperty': TReadOnlyProperty<string>;
   'meters': string;
+  'metersProperty': TReadOnlyProperty<string>;
   'distanceSceneAbsoluteDistanceTemplate': string;
+  'distanceSceneAbsoluteDistanceTemplateProperty': TReadOnlyProperty<string>;
   'distanceSceneDirectedPositiveDistanceTemplate': string;
+  'distanceSceneDirectedPositiveDistanceTemplateProperty': TReadOnlyProperty<string>;
   'distanceSceneDirectedNegativeDistanceTemplate': string;
+  'distanceSceneDirectedNegativeDistanceTemplateProperty': TReadOnlyProperty<string>;
   'degree': string;
+  'degreeProperty': TReadOnlyProperty<string>;
   'degrees': string;
+  'degreesProperty': TReadOnlyProperty<string>;
   'temperatureSceneAbsoluteDistanceTemplate': string;
+  'temperatureSceneAbsoluteDistanceTemplateProperty': TReadOnlyProperty<string>;
   'temperatureSceneDirectedPositiveDistanceTemplate': string;
+  'temperatureSceneDirectedPositiveDistanceTemplateProperty': TReadOnlyProperty<string>;
   'temperatureSceneDirectedNegativeDistanceTemplate': string;
+  'temperatureSceneDirectedNegativeDistanceTemplateProperty': TReadOnlyProperty<string>;
   'fish': string;
+  'fishProperty': TReadOnlyProperty<string>;
   'bird': string;
+  'birdProperty': TReadOnlyProperty<string>;
   'elevationSceneAbsoluteDistanceTemplate': string;
+  'elevationSceneAbsoluteDistanceTemplateProperty': TReadOnlyProperty<string>;
   'elevationSceneDirectedPositiveDistanceTemplate': string;
+  'elevationSceneDirectedPositiveDistanceTemplateProperty': TReadOnlyProperty<string>;
   'elevationSceneDirectedNegativeDistanceTemplate': string;
+  'elevationSceneDirectedNegativeDistanceTemplateProperty': TReadOnlyProperty<string>;
 };
 
 const numberLineDistanceStrings = getStringModule( 'NUMBER_LINE_DISTANCE' ) as StringsType;
