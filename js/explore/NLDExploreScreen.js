1af985f1b1e1f4526d48d13ac6b5e435f74d69d4
phet-dev <phetadmin@gmail.com>
2023-10-31 03:56:28 -0600

    update copyright dates from daily grunt work

--- js/explore/NLDExploreScreen.js
+++ js/explore/NLDExploreScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2023, University of Colorado Boulder
 
 /**
  * the 'Explore' screen in the Number Line: Distance simulation
