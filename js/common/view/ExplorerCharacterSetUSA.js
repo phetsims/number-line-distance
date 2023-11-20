bc6ad622467a5882ff5b0eab024cbab6945f8c9c
Marla Schulz <marlaa.schulz@gmail.com>
2023-11-20 12:41:40 -0600

    Switch USA person from mipmap to image, see: https://github.com/phetsims/number-line-distance/issues/72

--- js/common/view/ExplorerCharacterSetUSA.js
+++ js/common/view/ExplorerCharacterSetUSA.js
@@ -9,7 +9,7 @@
 
 import exploreHomeIcon_png from '../../../images/usa/exploreHomeIcon_png.js';
 import exploreNavbarIcon_png from '../../../images/usa/exploreNavbarIcon_png.js';
-import person_png from '../../../mipmaps/usa/person_png.js';
+import person_png from '../../../images/usa/person_png.js';
 import NumberLineDistanceStrings from '../../NumberLineDistanceStrings.js';
 import { USA_QUERY_VALUE } from '../NLDQueryParameters.js';
 import ExplorerCharacterSet from './ExplorerCharacterSet.js';
