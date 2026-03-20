e37d9d9a62dec3c4041515507fdcfd815cdce3b4
phet-dev <phetadmin@gmail.com>
2026-03-20 10:10:06 -0600

    update copyright dates from daily grunt work

--- js/common/model/NLDPreferences.js
+++ js/common/model/NLDPreferences.js
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2026, University of Colorado Boulder
 
 /**
  * NLD Preferences is the model that simulation specific preferences Properties.
